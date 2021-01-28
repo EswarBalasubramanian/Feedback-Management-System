26b0c834886eaedaa4ac05347d22395125bff03b
EswarBalasubramanian <eswarbtechit@gmail.com>
2021-01-28 10:28:01 +0530

    Add Procfile, PORT, gitignore

--- app.js
+++ app.js
@@ -383,6 +383,11 @@ app.post("/deleteone", function (req, res) {
   }
 });
 
-app.listen(3000, function () {
-  console.log("Server started on port 3000");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function () {
+  console.log("Server started on port " + port);
 });
